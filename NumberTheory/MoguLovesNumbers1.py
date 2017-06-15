cdd80aaacec484255248058fb6fe808e32d617e4
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-06-15 21:22:12 +0530

    mathematical solution not not reliable

--- NumberTheory/MoguLovesNumbers1.py
+++ NumberTheory/MoguLovesNumbers1.py
@@ -1,6 +1,7 @@
 """
 This solution of the problem will use the eratosthenes algorithm to generate prime numbers till r.
 """
+import math
 # def answer1(l, r):
 #     found=[]
 #     count=0
@@ -29,25 +30,53 @@ def answer(r=1000001):
             for n in range(i*i, r, i):     # Mark factors non-prime, remember that we are simply marking factors from i*i, not from i itself. This is important.
                 a[n] = False
 
-def answer1()
-
-answer()
-t=int(input())
-count=0
-while t>0:
-    l, r=input().split()
-    l=int(l)
-    r=int(r)
-    if l>r:
-        temp=l
-        l=r
-        r=temp
-    count=0
-    for i in range(l, r+1):
+def countPrimes(value): 
+    # count the number of prime numbers up to a
+    r=value
+    c=0
+    for i in range(r):
+        # print ('i: {} and isprime: {}'.format(i, isprime))
         if a[i]:
-            count+=1
-    print(count)
-    t-=1
+            c+=1
+            for n in range(i*i, r, i):     # Mark factors non-prime, remember that we are simply marking factors from i*i, not from i itself. This is important.
+                a[n] = False
+    return c
+
+def answer1(l):
+    """
+    One mathematical solution to find the number of primes upto a certain number is called the Prime Number Theorem. It says that the number of primes till the first N natural numbers is roughly equal to N/(log N) where log is the natural logarithm.
+    """
+    # this function tests how far the mathematical approximation gives correct results
+    numPrimesL=int(l/math.log(l))
+    actualNumPrimesL=countPrimes(l)
+    print('math approximation: {}, actual: {}'.format(numPrimesL, actualNumPrimesL))
+
+
+def test():
+    # testing the accuracy of mathematical approximation
+    for i in range(3, 10000):
+        print ('number: {} '.format(i),end='')
+        answer1(i)
+
+test()
+# # answer()
+# t=int(input())
+# # count=0
+# while t>0:
+#     l, r=input().split()
+#     l=int(l)
+#     r=int(r)
+#     if l>r:
+#         temp=l
+#         l=r
+#         r=temp
+#     print(answer1(l,r))
+#     # count=0
+#     # for i in range(l, r+1):
+#     #     if a[i]:
+#     #         count+=1
+#     # print(count)
+#     t-=1
 
 
             
